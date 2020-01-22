373788de34b179f64aa53a3c257d43dc6f59bad8
mana-murakami <mmurakami@nvidia.com>
2020-01-22 23:21:34 +0900

    modify job.sh

--- job.sh
+++ job.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-#PBS -q h-debug              # <-------------- 投入するキューを指定する
-#PBS -l select=2:mpiprocs=2:ompthreads=1  #<-- 実行したいノード数と、ノードあたりプロセス数を記述する（ここでは合計４プロセス）
-#PBS -W group_list=pz0xxx    # <-------------- 自分のグループ名に書き換える
-#PBS -l walltime=00:15:00    # <-------------- 実行時間上限を指定する
+#PBS -q h-tutorial
+#PBS -l select=2:mpiprocs=2:ompthreads=1
+#PBS -W group_list=gt00
+#PBS -l walltime=00:15:00
 
-GROUP=$(id -ng)
-MYDIR=/lustre/${GROUP}/${USER}
-export HOME=$MYDIR
 
-. $MYDIR/env.sh
+MYDIR=/lustre/pz0415/z30415
 
-cd $MYDIR
+. /etc/profile.d/modules.sh
+module load chainer/7.0.0
+module load openmpi/gdr/2.1.2/gnu
+export PYTHONUSERBASE=$MYDIR/.python
+
+cd $MYDIR/chainer-on-reedbush
 
 # デバッグ出力： Chainer と CuPy のバージョンを表示
-python -c "import chainer; print(chainer.__version__)"
-python -c "import cupy; print(cupy.cuda.nccl.get_version())"
+python -c "import chainer; print(chainer.print_runtime_info())"
 
 mpiexec -x PYTHONUSERBASE \
         python ./train_mnist.py -g --communicator pure_nccl
